d99e9926db756d6028abb2c7f3666930f33b9f79
WaiYanKyaw <waiyankyaw.dev@gmail.com>
2025-05-10 22:13:48 +0800

    one design file added

---
+++ RiscVCpu.srcs/sources_1/new/Decoder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/05/10 22:13:00
+// Design Name: 
+// Module Name: Decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Decoder(
+
+    );
+endmodule
